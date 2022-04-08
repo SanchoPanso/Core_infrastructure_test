aa576c129646094b83c002bb76bf1c5b55e3d10e
Alexander <chernikov.av@edu.spbstu.ru>
2022-04-08 22:28:16 +0300

    make func for sending and receiving

--- client/main.c
+++ client/main.c
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/unistd.h>
 
 #define BUF_SIZE 1024
@@ -11,7 +12,33 @@
 #define SERVER_PORT 9002
 #define SERVER_ADDR "127.0.0.1"
 #define FILE_PATH "../test.txt"
+#define SAVED_FILE_NAME "change.txt"
 char buf[BUF_SIZE];
+
+void send_message(int sock) {
+
+    // send filename
+    strcpy(buf, SAVED_FILE_NAME);
+    send(sock, buf, BUF_SIZE, 0);
+    // printf("%s", buf);
+
+    // send file data
+    FILE *in = fopen(FILE_PATH,"rb");
+    if (in == NULL) {
+        perror("file");
+        exit(1);
+    }
+
+    while(!feof(in)) {
+        unsigned long number_of_bytes = fread(buf,1, sizeof(buf), in);
+        if(number_of_bytes != 0) {
+            send(sock, buf, number_of_bytes,0);
+            // recv(sock, buf, number_of_bytes, 0);
+            // printf("%s", buf);
+        }
+    }
+    fclose(in);
+}
 
 int main()
 {
@@ -19,32 +46,27 @@ int main()
     struct sockaddr_in addr;
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock < 0)
-    {
+    if(sock < 0) {
         perror("socket");
         exit(1);
     }
 
+    struct in_addr in_p;
+    if (inet_aton(SERVER_ADDR, &in_p) == 0) {
+        perror("address");
+        exit(3);
+    }
+
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(SERVER_PORT); // или любой другой порт...
-    addr.sin_addr.s_addr = inet_addr(SERVER_ADDR); // htonl(INADDR_LOOPBACK);
+    addr.sin_port = htons(SERVER_PORT);
+    addr.sin_addr.s_addr = in_p.s_addr; // inet_addr(SERVER_ADDR);
     if(connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
         perror("connect");
         exit(2);
     }
 
-    FILE *in = fopen(FILE_PATH,"rb");   // обработать !!!
-
-    while(!feof(in)) {
-        unsigned long number_of_bytes = fread(buf,1, sizeof(buf), in);
-        if(number_of_bytes != 0) {
-            send(sock, buf, number_of_bytes,0);
-            recv(sock, buf, number_of_bytes, 0);
-            printf("%s", buf);
-        }
-    }
-    fclose(in);
+    send_message(sock);
 
     //printf("%s", buf);
     close(sock);
--- server/main.c
+++ server/main.c
@@ -3,21 +3,55 @@
 #include <netinet/in.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <sys/unistd.h>
 
 #define BUF_SIZE 1024
-#define SAVE_DIR ".."
+#define SAVE_DIR "../"
 #define FILE_NAME "test.txt"
 
 #define SERVER_PORT 9002
 #define SERVER_ADDR "127.0.0.1"
 
+char buf[BUF_SIZE];
+
+void process_message(int sock) {
+
+    long bytes_read = 0;
+    printf("Start processing received message...\n");
+
+    // receive filename
+    char filename[BUF_SIZE];
+    bytes_read = recv(sock, filename, BUF_SIZE, 0);
+    if (bytes_read <= 0)
+        return;
+    char filepath[BUF_SIZE] = SAVE_DIR;
+    strcat(filepath, filename);
+    printf("File will be saved in \"%s\"\n", filepath);
+
+    // receive file data
+    FILE *out = fopen(filepath, "wb");
+    if (out == NULL) {
+        perror("file");
+        exit(1);
+    }
+
+    while(true) {
+        bytes_read = recv(sock, buf, BUF_SIZE, 0);
+        if (bytes_read <= 0) break;
+        fwrite(buf, 1, bytes_read, out);
+        //send(sock, buf, bytes_read, 0);
+    }
+    fclose(out);
+    printf("File saved\n");
+}
+
 int main()
 {
     int sock, listener;
     struct sockaddr_in addr;
-    char buf[BUF_SIZE];
     long bytes_read;
 
     listener = socket(AF_INET, SOCK_STREAM, 0);
@@ -27,35 +61,32 @@ int main()
         exit(1);
     }
 
+    struct in_addr in_p;
+    if (inet_aton(SERVER_ADDR, &in_p) == 0) {
+        perror("address");
+        exit(3);
+    }
+
     addr.sin_family = AF_INET;
     addr.sin_port = htons(SERVER_PORT);
-    addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);   //htonl(INADDR_ANY);
+    addr.sin_addr.s_addr = in_p.s_addr;//inet_addr(SERVER_ADDR);
     if(bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
         perror("bind");
         exit(2);
     }
 
+    printf("Listening...\n");
     listen(listener, 1);
 
     while(1)
     {
         sock = accept(listener, NULL, NULL);
-        if(sock < 0)
-        {
+        if(sock < 0) {
             perror("accept");
             exit(3);
         }
-
-        FILE *out = fopen(SAVE_DIR "/" FILE_NAME, "wb");
-        while(1)
-        {
-            bytes_read = recv(sock, buf, BUF_SIZE, 0);
-            if(bytes_read <= 0) break;
-            fwrite(buf, 1, bytes_read, out);
-            send(sock, buf, bytes_read, 0);
-        }
-        fclose(out);
+        process_message(sock);
         close(sock);
         close(listener);
         break;
